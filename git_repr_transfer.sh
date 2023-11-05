f0bd81333e3bef35c91b16a3659fd3bfc83b1974
Mahbub Talukder <mahbub.alum@cg-bd.com>
2023-11-05 11:56:23 +0600

    test

--- git_repr_transfer.sh
+++ git_repr_transfer.sh
@@ -2,3 +2,11 @@ git remote set-url origin https://github.com/ripandevops/django-unified-portal.g
 git push origin --all
 git push origin --tags
 
+#unified frontend
+git remote set-url origin https://github.com/ripandevops/unified-portal-front-end.git https://mahbub_cgbd@bitbucket.org/RipanBasakCg/unified-portal-front-end.git
+#odoo-11 docker
+git remote set-url origin https://github.com/ripandevops/odoo-11-docker.git https://mahbub_cgbd@bitbucket.org/RipanBasakCg/odoo-11-docker.git
+
+git commit --amend --author="Mahbub Talukder <mahbubalum.talukder@gmail.com>"
+
+
